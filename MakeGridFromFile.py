596f2aa8c116a21b6526c85de9736e6df0eacda4
Tor <torkbo@stud.ntnu.no>
2026-09-11 11:41:43 +0200

    La inn nordic490 kartet

--- MakeGridFromFile.py
+++ MakeGridFromFile.py
@@ -1,6 +1,19 @@
+import os
 from pathlib import Path
 import pandas as pd
 import random
+import sys
+import numpy as np
+
+#--------------------Hente filstien til megselv, må øke til mer--------
+current_path = os.getcwd()
+current_path=Path(current_path)
+TorKodePath=current_path
+TorKodePath=TorKodePath / "TorKode"
+sys.path.append(str(TorKodePath))
+from theveninZth import theveninZth
+#--------------------Hente filstien til megselv, må øke til mer--------
+
 
 FolderName="Grid"
 
@@ -32,22 +45,10 @@ class Grid:
       self.line = []       # list of line objects
 
 
-   def theveninZth(a,b):
-      #lagre a og b som bus indeks som er funnet
-      #finn alle linjene tilkoblet bus a og bus b
-      #lagre andre siden av bussene i indeksen som er funnet
-      #så indeksen er [a,b] hvor a er start og b er slutt
-      #Så går vi kun ut av a(start), og looper gjennom endepunktene på andre siden av de  linjene som er koblet til a
-      #Dette blir så [a,b,[a,[b,c]],[c,[d]],[d,[b]]]
-      #Siden b er funnet i indeks 1, så er den grenen ferdig, så vi fortsetter nå kun fra c
-      #c går til d, og d går til b, som da sier at den er ferdig.
-      #Det lages så to lister, serier og parrareller, som er tomme foreløbig
-      #Vi leser så fra denne [a,b,[a,[b,c]],[c,[d]],[d,[b]]]
-      #serier [[a,b],[a,c,d,b]]
-      #parrareller [[a,b],[a,c,d,b]]
-      
+   def thevenin(self,bus1,bus2):
+      return theveninZth(self,bus1,bus2)
 
-      return Zth
+
 #----------Base values start----------------
    class BaseValues:
       def __init__(self,Sbase,Vbase):
@@ -103,6 +104,9 @@ class Grid:
 
       def admittans(self):
          return 1/complex(self.R, self.X)
+
+      def impedans(self):
+         return 1/complex(self.R,self.X)
    
 #----------lines end----------------------
 #----------Making a grid from xslx end----
---
+++ TorKode/theveninZth.py
@@ -0,0 +1,42 @@
+def theveninZth(grid,bus1,bus2):
+
+   #------oppstart-----
+   mainlist=[]
+   mainlist.append(bus1)
+   mainlist.append(f"end {bus2}")
+   index=[]
+   index.append(0)
+   index.append(0)
+   #------oppstart-----
+
+
+
+
+   #par er enkelt linjer
+   #så må ta en loop for alle frombus og tilbus
+
+   #---------leter etter linjer koblet til node i--------
+   def connections(node):
+      midlertidig=[]
+      midlertidigindex=[]
+      for l in grid.line:
+         print(l)
+         if l.Frombus==node:
+            midlertidig.append(l.Tobus)
+            midlertidigindex.append(f"1+{l.Tobus}")
+         if l.Tobus==node:
+            midlertidig.append(l.Frombus)
+            midlertidigindex.append(f"1+{l.Frombus}")
+      mainlist.append([node,midlertidig])
+      index.append([node,midlertidigindex])
+
+   connections(bus1)
+
+
+   
+   #---------leter etter linjer koblet til node i --------
+
+
+
+   return print("mainlist:",mainlist,"index:",index)
+    
