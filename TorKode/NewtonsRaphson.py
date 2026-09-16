bd5e295eff3b01cc03f9ff65ac61818069399806
Tor <torkbo@stud.ntnu.no>
2026-09-16 22:26:10 +0200

    mellomlagrer snart feridg med NR

--- TorKode/NewtonsRaphson.py
+++ TorKode/NewtonsRaphson.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def NewtonRaphson(grid): #Furuseth er ref
 
+   #---------oppretter klasser PV og PQ for bussene--------------------
    def busclassifier_with_voltage_and_delta_vektor(grid):
       bus=[]
       for i in range(len(grid.bus)):
@@ -15,10 +16,11 @@ def NewtonRaphson(grid): #Furuseth er ref
          if PV==False:
             bus.append("PQ")
       return bus
+   #---------oppretter klasser PV og PQ for bussene--------------------
          
    busPVPQ=busclassifier(grid)
 
-
+   #-----------lastflytligningen for aktiv effekt---------------------
    def powerflowequationP(V_i,V_j,Y,delta_i,delta_j,grid):
       V_i=np.abs(V_i)
       V_j=np.abs(V_i)
@@ -27,8 +29,9 @@ def NewtonRaphson(grid): #Furuseth er ref
       delta_j=delta_j
       theta_ij=np.angel(Y[i,j])
       return V_i*V_j*Yij*np.cos(delta_i-delta_j-theta_ij)
+   #-----------lastflytligningen for aktiv effekt---------------------
 
-   
+   #-----------lastflytligningen for reaktiv effekt---------------------
    def powerflowequationQ(V_i,V_j,Y,delta_i,delta_j,grid):
       V_i=np.abs(V_i)
       V_j=np.abs(V_i)
@@ -37,7 +40,9 @@ def NewtonRaphson(grid): #Furuseth er ref
       delta_j=delta_j
       theta_ij=np.angel(Y[i,j])
       return V_i*V_j*Yij*np.sin(delta_i-delta_j-theta_ij)
+   #-----------lastflytligningen for reaktiv effekt---------------------
 
+   #-------------Løser P=Pgen-Pload og Q=Qgen-Qload for gridet------------------------
    def power_scheduled(busPVPQ,grid):
       P_scheduled=[]
       Q_scheduled=[]
@@ -51,9 +56,12 @@ def NewtonRaphson(grid): #Furuseth er ref
             Q_mid=grid.bus[i].Q_gen-grid.bus[i].Q_load
             Q_scheduled.append(Q_mid)
       return P_scheduled, Q_scheduled
-   
+   #-------------Løser P=Pgen-Pload og Q=Qgen-Qload for gridet------------------------
+
+
    P_scheduled, Q_scheduled=power_scheduled(busPVPQ,grid)
 
+   #-----------------Her er Newtons Raphson metoden, her er kjernen-------------------
    def totalsolver(grid,busPVPQ,P_scheduled, Q_scheduled):
       iterasjon=0
       deltaPQ=[]
@@ -68,25 +76,110 @@ def NewtonRaphson(grid): #Furuseth er ref
             rows=rows+2
 
       
-      
+      #---------------Lager jacobian matrisen----------------
       def jacobian():  #er har jeg håndregnet ut generelle definisjoner på de deriverte, jobber er å få bygd opp matrisen på et hvilke som helst system.
+        
+         J = np.zeros((rows, rows))
          Y=grid.admittansmatrise()
-
-          def dpi_ddeltai(i):      #i er da definert for hvilke pi/deltai vi jobber med
+          
+          #----------den paritell deriverte pi med hensyn på angle i-------------------
+          def dpi_ddeltai(i):
             a=0
             for j in range(len(grid.bus)-1):
                if j!=i:
-                  a=a-np.abs(grid.bus[i].Volt)*np.abs(grid.bus[j].Volt)*np.abs(Y[i,j])*np.cos(grid.bus[i].Angle-grid.bus[i].Angle-np.angle(Y[i,j]))  
+                  a=a-np.abs(grid.bus[i].Volt)*np.abs(grid.bus[j].Volt)*np.abs(Y[i,j])*np.sin(grid.bus[i].Angle-grid.bus[i].Angle-np.angle(Y[i,j]))  
             return a
+         #----------den paritell deriverte pi med hensyn på angle i-------------------
 
+         #----------den paritell deriverte pi med hensyn på angle j-------------------
+         def dpi_ddeltaj(i,j):
+            a = np.abs(grid.bus[i].Volt)*np.abs(grid.bus[j].Volt)*np.abs(Y[i,j])*np.sin(grid.bus[i].Angle-grid.bus[j].angle-np.angle(Y[i,j]))
+            return a
+         #----------den paritell deriverte pi med hensyn på angle j-------------------
+
+         #----------den paritell deriverte pi med hensyn på volt i-------------------
+         def dpi_dvi(i):
+            a=0
+            for j in range(len(grid.bus)-1):
+               if j!=i:
+                  a=a+np.abs(grid.bus[i].Volt)*np.abs(grid.bus[j].Volt)*np.abs(Y[i,j])*np.cos(grid.bus[i].Angle-grid.bus[i].Angle-np.angle(Y[i,j]))  
+               if j==i:
+                  a=a+2*np.abs(grid.bus[i].Volt)*np.abs(Y[i,j])*np.cos(grid.bus[i].Angle-grid.bus[j].angle-np.angle(Y[i,j]))
+            return a
+         #----------den paritell deriverte pi med hensyn på volt i-------------------
          
 
+         #----------den paritell deriverte pi med hensyn på volt j-------------------
+         def dpi_dvj(i):
+            a = np.abs(grid.bus[i].Volt)*np.abs(Y[i,j])*np.cos(grid.bus[i].Angle-grid.bus[j].angle-np.angle(Y[i,j]))
+            return a
+         #----------den paritell deriverte pi med hensyn på volt j-------------------
+
+
+         #----------den paritell deriverte qi med hensyn på angle i-------------------
+          def dqi_ddeltai(i):
+            a=0
+            for j in range(len(grid.bus)-1):
+               if j!=i:
+                  a=a + np.abs(grid.bus[i].Volt)*np.abs(grid.bus[j].Volt)*np.abs(Y[i,j])*np.cos(grid.bus[i].Angle-grid.bus[i].Angle-np.angle(Y[i,j]))  
+            return a
+         #----------den paritell deriverte qi med hensyn på angle i-------------------
+
+         #----------den paritell deriverte qi med hensyn på angle j-------------------
+         def dqi_ddeltaj(i,j):
+            a = -1 * np.abs(grid.bus[i].Volt)*np.abs(grid.bus[j].Volt)*np.abs(Y[i,j])*np.cos(grid.bus[i].Angle-grid.bus[j].angle-np.angle(Y[i,j]))
+            return a
+         #----------den paritell deriverte qi med hensyn på angle j-------------------
+
+         #----------den paritell deriverte qi med hensyn på volt i-------------------
+         def dqi_dvi(i):
+            a=0
+            for j in range(len(grid.bus)-1):
+               if j!=i:
+                  a=a+np.abs(grid.bus[i].Volt)*np.abs(grid.bus[j].Volt)*np.abs(Y[i,j])*np.sin(grid.bus[i].Angle-grid.bus[i].Angle-np.angle(Y[i,j]))  
+               if j==i:
+                  a=a+2*np.abs(grid.bus[i].Volt)*np.abs(Y[i,j])*np.sin(grid.bus[i].Angle-grid.bus[j].angle-np.angle(Y[i,j]))
+            return a
+         #----------den paritell deriverte qi med hensyn på volt i-------------------
+         
+
+         #----------den paritell deriverte qi med hensyn på volt j-------------------
+         def dqi_dvj(i):
+            a = np.abs(grid.bus[i].Volt)*np.abs(Y[i,j])*np.sin(grid.bus[i].Angle-grid.bus[j].angle-np.angle(Y[i,j]))
+            return a
+         #----------den paritell deriverte qi med hensyn på volt j-------------------
+
+
+         #-----------------bygge jacobian matrisen utifra funksjonene over----------
+         for i in range(rows):
+            k=0#dummy variabel siden pq legger til 2 rader istedenfor 1 i pv
+            for j in range(rows):
+               if buPVPQ[i]=="PV"
+                  if busPVPQ[j]=="PV":
+                     if j==k:
+                        J[k,j]=dpi_ddeltai(k):
+                     else:
+                        J[k,j]=dpi_ddeltaj(k,j)
+                  k=k+1
+                  else:
+                     if j==k:
+                        J[k,j]=dpi_ddeltai(i):
+                        J[k+1,j]=dqi_ddeltai(i)
+                     else:
+                        J[k,j]=dpi_ddeltaj(i,j)
+                        J[k,j]=dqi_ddeltaj(i,j)
+               else:
+                  
+          #-----------------bygge jacobian matrisen utifra funksjonene over----------
+
+
+                  
+      return J
 
 
 
 
-      def calculate_deltaunkown_one_iterasjon(grid,rows,N,deltaPQ,iterasjon):
-         return 0
+      #---------------Lager jacobian matrisen----------------
 
 
       def calculate_deltaPQ_one_iterasjon(grid,rows,N,deltaPQ,iterasjon):
@@ -122,24 +215,5 @@ def NewtonRaphson(grid): #Furuseth er ref
 
 
 
-
-
-      
-
-      
-
-
-
-
-
-
-
-   
-      
-
-
-
-   
-
-
    return bus
+    #-----------------Her er Newtons Raphson metoden, her er kjernen-------------------
